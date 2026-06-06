909e392da4fff43887d266d60338cd253a44b8b6
Elle <115524533+hexagonelle@users.noreply.github.com>
2026-06-06 14:31:23 -0700

    Added correct script for recall gem

--- kubejs/startup_scripts/custom-recallgem.js
+++ kubejs/startup_scripts/custom-recallgem.js
@@ -1,23 +1,42 @@
-PlayerEvents.respawned(event => {
-    const data = event.player.persistentData;
-    let deathLoc = event.player.nbt.LastDeathLocation;
-
-    // event.player.tell("Welcome Back");
-    // event.player.tell("Last death: " + deathLoc.pos);
-    // event.player.tell("DeathDim: " + deathLoc.dimension);
-
-    let recallGem =
-        Item.of(
-            'kubejs:recall_gem',
-            {
-                "coords":{
-                    "x": deathLoc.pos[0],
-                    "y": deathLoc.pos[1],
-                    "z": deathLoc.pos[2]
-                },
-                "dimension": deathLoc.dimension
-            }
-        ).weakNBT();
-
-    event.player.give(recallGem);
+// Listen to item registry event
+StartupEvents.registry('item', event => {
+	event.create('recall_gem')
+		.maxStackSize(1)
+		.glow(true)
+		.useAnimation('bow')
+		.useDuration(itemstack => 64)
+	.use((level, player, hand) => true)
+	.finishUsing((itemstack, level, entity) => {
+		if(itemstack.nbt != null){
+			let coords = itemstack.nbt.coords;
+			let dim = itemstack.nbt.dimension;
+			itemstack.shrink(1)
+			if(itemstack.nbt.coords != null){
+				if (entity.player) {
+					// entity.tell("Coords are: " + coords.x);
+					entity.teleportTo(
+						dim,
+						coords.x, coords.y, coords.z,
+						0.0,0.0
+					);
+				let potion = entity.potionEffects
+				
+				//potion.add(effect, duration, amplifier, ambient, showParticles)
+				potion.add(
+					'minecraft:resistance',
+					1200, 5, true, false
+				)
+				potion.add(
+					'minecraft:invisibility',
+					1200, 5, true, false
+				)
+				potion.add(
+					'minecraft:water_breathing',
+					1200, 5, true, false
+				)
+				}
+			}
+		}
+		return itemstack
+	})
 })
